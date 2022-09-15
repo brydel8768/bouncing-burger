6eaf67b26caec758c26a7ccca73290f8c72147f8
brydel8768 <112518840+brydel8768@users.noreply.github.com>
2022-09-15 14:50:30 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,85 @@
+namespace SpriteKind {
+    export const monl = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.monl, SpriteKind.Food, function (sprite, otherSprite) {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.monl, SpriteKind.Player, function (sprite, otherSprite) {
+    game.over(false)
+})
+let barger = sprites.create(img`
+    . . . . c c c b b b b b . . . . 
+    . . c c b 4 4 4 4 4 4 b b b . . 
+    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+    8 7 2 e e e e e e e e e e 2 7 8 
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+    e b e 8 8 c c 8 8 c c c 8 e b e 
+    e e b e c c e e e e e c e b e e 
+    . e e b b 4 4 4 4 4 4 4 4 e e . 
+    . . . c c c c c e e e e e . . . 
+    `, SpriteKind.Player)
+barger.vx = 60
+barger.vy = 100
+barger.setBounceOnWall(true)
+let doggie = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.monl)
+doggie.setPosition(28, 85)
+controller.moveSprite(doggie)
+let taco = sprites.create(img`
+    ..............eeeeeee...........
+    ............ee455662e2e.........
+    ..........ee45556723e2688.......
+    .........e46776677232e777668....
+    ........e46745554772227776778...
+    .......4448744444777766777678...
+    ......4522e7777776777766676668..
+    .....4523227766722e666eeeee888..
+    ....455232e76672322e4555dddd48..
+    ...44567777554623e455ddddddddd4.
+    ...e66774554477e455dddd55554dd44
+    ..e46777444677e55dd55555d55dddd4
+    ..e5668677666e5dd555555555555dde
+    .e45544e8776e5d555554555555555de
+    .e554eeee66e5d5555d55555dddd54de
+    .e55ee44fee5d5d555555d5d5dddddde
+    e454eeeefe45d55555555555dd4ddde.
+    e5e4eefffe5d55555555d5555dddde..
+    e5ee4eeff45d555555555555dddde...
+    e5eeeeffe5d55d555d5555d5ddde....
+    e5ffefeee5d55545555555ddd4e.....
+    e5ffffffe545555555d5d4ddee......
+    e54efeff45d55d55555dddde........
+    e5eeeffe5dd5555545dddee.........
+    e4eeefff5d5555d55ddde...........
+    e4efefff5d5d55555d4e............
+    .e4efffe5d555555dee.............
+    .e54eeee5d545dd4e...............
+    ..e554ee5dddddee................
+    ...ee5544dddee..................
+    .....eeeeeee....................
+    ................................
+    `, SpriteKind.Food)
+taco.setPosition(139, 15)
